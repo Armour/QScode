2b99433aeb0409b1f49ebcfa3c742e468188556f
Armour <497052684@qq.com>
2015-05-03 18:39:50 +0800

    Initial Commit

---
+++ QScode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QScode
+//
+//  Created by Armour on 5/3/15.
+//  Copyright (c) 2015 ZJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QScode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QScode
+//
+//  Created by Armour on 5/3/15.
+//  Copyright (c) 2015 ZJU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
